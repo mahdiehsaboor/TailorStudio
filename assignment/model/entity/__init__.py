9804a544a103ed39a170d0de019f3a3380e0c158
mahdiehsaboor <167888915+mahdiehsaboor@users.noreply.github.com>
2024-06-13 21:10:27 +0330

    Update __init__.py

--- assignment/model/entity/__init__.py
+++ assignment/model/entity/__init__.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import relationship
 import re
 
 from assignment.model.entity.base import Base
-from assignment.model.entity.customer import Customer
+from assignment.model.entity.customer import Customer # to do
 from assignment.model.entity.tailor import Tailor
 from assignment.model.entity.order import Order
 from assignment.model.entity.clothes import Clothes
