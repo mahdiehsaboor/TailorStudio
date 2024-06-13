c057d0796cb00e95c71a7fee93482791b47b37e2
mahdiehsaboor <167888915+mahdiehsaboor@users.noreply.github.com>
2024-06-13 20:57:34 +0330

    Update __init__.py

--- assignment/controller/__init__.py
+++ assignment/controller/__init__.py
@@ -1,5 +1,5 @@
 from assignment.controller.exceptions.customer_exceptions import *
-from assignment.controller.customer_controller import  CustomerController
+from assignment.controller.customer_controller import  CustomerController # to do 
 from assignment.controller.tailor_controller import TailorController
 from assignment.controller.order_controller import OrderController
 from assignment.controller.clothes_controller import ClothesContoller
